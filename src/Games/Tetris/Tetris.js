dba64a3dbe817374d0bbf6d7350e3efcb92c7827
Dhroov Makwana <pabloescoder@gmail.com>
2022-08-13 03:13:59 +0530

    Add functions to handle tetromino movement
    - Move tetromino on arrow key press, and re-render the stage after every movement
    - Clear the stage and reset player state when start game is clicked

--- src/Games/Tetris/Tetris.js
+++ src/Games/Tetris/Tetris.js
@@ -4,6 +4,7 @@ import { useStage } from "../../hooks/useStage";
 import Stage from "./Stage";
 import Display from "./Display";
 import StartButton from "./StartButton";
+import { createStage } from "./gameHelpers";
 
 import styles from "./Tetris.module.css";
 
@@ -11,11 +12,46 @@ const Tetris = () => {
   const [dropSpeed, setDropSpeed] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    // Reset
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = (keyCode) => {
+    if (!gameOver) {
+      if (keyCode === 37) {
+        movePlayer(-1); // Move to the left
+      } else if (keyCode === 39) {
+        movePlayer(1); // Move to the right
+      } else if (keyCode === 40) {
+        dropPlayer(); // Move down
+      }
+    }
+  };
+
   return (
-    <div className={styles["tetris-wrapper"]}>
+    <div
+      className={styles["tetris-wrapper"]}
+      role="button"
+      tabIndex="0"
+      onKeyDown={(e) => move(e.keyCode)}
+    >
       <div className={styles.tetris}>
         <Stage stage={stage} />
         <aside className={styles["aside-container"]}>
@@ -28,7 +64,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton callback={startGame} />
         </aside>
       </div>
     </div>
--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,7 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { createStage } from "../Games/Tetris/gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // Clear the stage which was rendered previously
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      // Draw the tetromino on the new stage
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+      return newStage;
+    };
+    setStage((prev) => updateStage(prev));
+  }, [player]);
   return [stage, setStage];
 };
