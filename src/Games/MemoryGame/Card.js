cbe656b858d16454288d824a1a115c0cb7cf7837
Aniket Ingle <aniketingle072002@gmail.com>
2022-08-13 19:54:59 +0530

    checking correct and wrong cards

--- src/Games/MemoryGame/Card.js
+++ src/Games/MemoryGame/Card.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styles from "./MemoryGame.module.css";
-function Card({ item }) {
+function Card({ item, id, handleClick }) {
+  const itemClass = item.stat ? item.stat : "";
   return (
-    <div className={styles.card}>
+    <div
+      className={styles.card}
+      id={styles[`${itemClass}`]}
+      onClick={() => handleClick(id)}
+    >
       <img src={item.img} alt={item.id} />
     </div>
   );
