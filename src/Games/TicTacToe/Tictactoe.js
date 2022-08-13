58be68df29c63a02c85e4bda3c127457bf5e4c37
Aniket Ingle <aniketingle072002@gmail.com>
2022-08-13 13:28:41 +0530

    checking winner in tictactoe

--- src/Games/TicTacToe/Tictactoe.js
+++ src/Games/TicTacToe/Tictactoe.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import SquareComponent from "./SquareComponent";
 import styles from "./Tictactoe.module.css";
 const initialState = ["", "", "", "", "", "", "", "", ""];
@@ -10,6 +10,38 @@ function Tictactoe() {
     strings[index] = isXChance ? "X" : "O";
     updateGameState(strings);
     updateIsXChance(!isXChance);
+  };
+  useEffect(() => {
+    const winner = checkWinner();
+    if (winner) {
+      setTimeout(() => {
+        alert(`Ta da! ${winner} has won the game`);
+        updateGameState(initialState);
+      }, 100);
+    }
+  }, [gameState]);
+  const checkWinner = () => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        gameState[a] &&
+        gameState[a] === gameState[b] &&
+        gameState[a] === gameState[c]
+      ) {
+        return gameState[a];
+      }
+    }
+    return null;
   };
   return (
     <div className={styles["tictactoe-header"]}>
