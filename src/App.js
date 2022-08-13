2c0e2ffbd483b4994248c524a91f1d4b9333819a
Dhroov Makwana <pabloescoder@gmail.com>
2022-08-14 01:56:38 +0530

    Add WebApp name, add button to go to music component, embed music player

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import Homepage from "./components/Homepage";
 
 // Required for NextUI to work correctly
 import { NextUIProvider } from "@nextui-org/react";
-import FlappyBird from "./Games/FlappyBird/FlappyBird";
 import "./App.css";
 
 function App() {
   return (
-    // <NextUIProvider>
-    <div className="App">
-      {/* <Homepage /> */}
-      <FlappyBird />
-    </div>
-    // </NextUIProvider>
+    <NextUIProvider>
+      <div className="App">
+        <Homepage />
+      </div>
+    </NextUIProvider>
   );
 }
 
---
+++ src/components/MusicComponent.js
@@ -0,0 +1,18 @@
+const MusicComponent = () => {
+  return (
+    <div style={{ minHeight: "100vh" }}>
+      <iframe
+        style={{ margin: "2rem", marginLeft: "10%", borderRadius: "20px" }}
+        title="spotify embed"
+        src="https://open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M"
+        width="80%"
+        height="600px"
+        frameBorder="0"
+        allowtransparency="true"
+        allow="encrypted-media"
+      ></iframe>
+    </div>
+  );
+};
+
+export default MusicComponent;
