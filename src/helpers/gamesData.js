6318f1b9fa5b67329d606069d9c812504ce63a6f
Dhroov Makwana <pabloescoder@gmail.com>
2022-08-14 02:02:50 +0530

    Replace Snakes and Ladders with Flappy Bird Card

--- src/helpers/gamesData.js
+++ src/helpers/gamesData.js
@@ -3,12 +3,12 @@ import tetrisCardImg from "../img/tetrisCard.jpg";
 import snakeCardImg from "../img/snakeCard.jpg";
 import memoryCardImg from "../img/memoryCard.jpg";
 import ticTacToeCardImg from "../img/ticTacToeCard.png";
-import snakesAndLaddersCardImg from "../img/snakesAndLaddersCard.png";
+import flappyBirdCardImg from "../img/flappyBirdCard.jpg";
 
 export const games = [
   { image: tetrisCardImg, title: "Tetris" },
   { image: snakeCardImg, title: "Snake" },
   { image: memoryCardImg, title: "Memory Game" },
   { image: ticTacToeCardImg, title: "Tic Tac Toe" },
-  { image: snakesAndLaddersCardImg, title: "Snakes & Ladders" },
+  { image: flappyBirdCardImg, title: "Flappy Bird" },
 ];
