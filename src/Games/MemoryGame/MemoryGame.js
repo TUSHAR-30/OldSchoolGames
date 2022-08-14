e1a9a2698907ce18c7dfb8992df1035a1e506803
Aniket Ingle <aniketingle072002@gmail.com>
2022-08-14 14:01:04 +0530

    new game button added to memory game

--- src/Games/MemoryGame/MemoryGame.js
+++ src/Games/MemoryGame/MemoryGame.js
@@ -1,8 +1,6 @@
 import React from "react";
 import Cards from "./Cards";
 import BackButton from "../../components/BackButton";
-import StartButton from "../../components/StartButton";
-import { Container } from "@nextui-org/react/";
 import styles from "./MemoryGame.module.css";
 
 function MemoryGame() {
@@ -13,16 +11,6 @@ function MemoryGame() {
         <div>
           <h1 className={styles.heading}>Memory Game</h1>
           <Cards />
-          <Container
-            display="flex"
-            justify="center"
-            css={{ width: "100%", margin: "2rem 0" }}
-          >
-            <StartButton
-              callback={() => console.log("Button Clicked")}
-              buttonText="New Game"
-            />
-          </Container>
         </div>
       </div>
     </>
